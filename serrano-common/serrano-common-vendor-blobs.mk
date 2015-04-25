7fb58469fbcf6cf6822ea1de10e9b33c1ee31d54
doc <doc.divxm@gmail.com>
2015-04-25 22:26:11 +0300

    serrano-common: Add lights.msm8960.so as prebuilt and fix BLN
    Lib source: f4k kernel
    
    Change-Id: If1c8370941eb111e937dc822ec67f2f967fc4ee4

--- serrano-common/serrano-common-vendor-blobs.mk
+++ serrano-common/serrano-common-vendor-blobs.mk
@@ -84,6 +84,7 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/serrano-common/proprietary/lib/hw/sensors.msm8960.so:system/lib/hw/sensors.msm8960.so \
     vendor/samsung/serrano-common/proprietary/bin/mm-qcamera-daemon:system/bin/mm-qcamera-daemon \
     vendor/samsung/serrano-common/proprietary/lib/hw/camera.vendor.msm8960.so:system/lib/hw/camera.vendor.msm8960.so \
+    vendor/samsung/serrano-common/proprietary/lib/hw/lights.msm8960.so:system/lib/hw/lights.msm8960.so \
     vendor/samsung/serrano-common/proprietary/lib/libadsprpc.so:system/lib/libadsprpc.so \
     vendor/samsung/serrano-common/proprietary/lib/libchromatix_imx175_antishake.so:system/lib/libchromatix_imx175_antishake.so \
     vendor/samsung/serrano-common/proprietary/lib/libchromatix_imx175_default_video.so:system/lib/libchromatix_imx175_default_video.so \
